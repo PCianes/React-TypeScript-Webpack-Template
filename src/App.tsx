627bf7149d17e57e20cf0a771340d02cfbca27fd
Pablo Cianes <cianespablo@gmail.com>
2021-03-24 08:37:18 +0100

    ✨ allow add styles, images and SVG files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import "./styles.css";
+import IMAGE from "./computer.jpg";
+import LOGO from "./logo.svg";
+
 export const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Image</p>
+      <img src={IMAGE} alt="test image" />
+      <p>SVG</p>
+      <img src={LOGO} alt="test logo" width="100" />
+    </>
+  );
 };
