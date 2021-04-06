523ee888b879f5c6f5b8276489b0cb8e429ad378
Pablo Cianes <cianespablo@gmail.com>
2021-04-06 09:05:46 +0200

    ⚙ fix dependencies & add copy plugin &  analyzer

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -41,5 +42,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
-  ]
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, '..', './src/source'), to: path.resolve(__dirname, '..', './build/dest') },
+      ]
+    }),
+  ],
 }
